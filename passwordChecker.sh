8edb553d899ab8720309bdabea2d260e085d480d
Sarahcarter97 <15sca@queensu.ca>
2016-11-13 14:22:48 -0500

    Update passwordChecker.sh

--- passwordChecker.sh
+++ passwordChecker.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+
+read -p "Please enter a password:" -s password
+LEN=${#password};
+
+#Does  the password have at least 8 characters?
+if [ $LEN -lt 8 ] ; then
+echo "Password is too short, you need a minimum of 8 characters"
+
+else
+echo "Password is 8 or more characters! This is goos"
+fi

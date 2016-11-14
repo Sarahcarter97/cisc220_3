925d79b88de43db8387e1f3f6fccc011905cc897
kateduquesnay <13khd@queensu.ca>
2016-11-14 18:11:33 -0500

    Update tpircs.sh

--- tpircs.sh
+++ tpircs.sh
@@ -1,12 +1,21 @@
 
-string="yahoo"
+
+read -p "Insert a list of words:" -a varArray
+
+newarray=()
+
+for string in ${varArray[*]}
+do
 mysize=${#string}
 i=$(expr $mysize - 1)
 newstring=""
-
 for ((n=0; n<${#string}; n++))
 do
 tempchar=${string:$i:1}
 newstring+=$tempchar
 ((i--))
 done
+newarray+=($newstring)
+done
+
+echo "Output: " ${newarray[*]}

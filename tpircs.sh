8e0510b1cc24fe40cb4af4415d3c15bed7d431e1
kateduquesnay <13khd@queensu.ca>
2016-11-14 18:16:40 -0500

    Update tpircs.sh

--- tpircs.sh
+++ tpircs.sh
@@ -1,5 +1,11 @@
 
+#Dean Wilkins-Reeves 10176758
+#Sarah Carter 10193595
+#Adele McCallum 10093867
+#Katherine DuQuesnay 10138898
+#Takes in a list of words from a user, and outputs an array with each word backwords
 
+#create new array containing words inputted by user
 read -p "Insert a list of words:" -a varArray
 
 newarray=()
@@ -12,9 +18,11 @@ newstring=""
 for ((n=0; n<${#string}; n++))
 do
 tempchar=${string:$i:1}
+#add last element (character) to the new string and decrease i
 newstring+=$tempchar
 ((i--))
 done
+
 newarray+=($newstring)
 done
 

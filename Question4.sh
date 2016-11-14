c23aec0eff7623c1b53270a457b3fad8032480b9
kateduquesnay <13khd@queensu.ca>
2016-11-14 18:34:10 -0500

    Update Question4.sh

--- Question4.sh
+++ Question4.sh
@@ -1,1 +1,9 @@
+#Assignment 3 Question 4
+#Dean Wilkins-Reeves 10176758
+#Sarah Carter 10193595
+#Adele McCallum 10093867
+#Katherine DuQuesnay 10138898
 
+#try to sort only the third column: ie the size
+lsblk --noheadings --raw | sort -k3n
+#we only want top 2, which will only be in G, not M

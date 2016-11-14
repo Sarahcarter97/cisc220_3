9d49cf72f74b8d621c62b8407cb9d840a7d084a2
kateduquesnay <13khd@queensu.ca>
2016-11-14 18:35:30 -0500

    Update Question4.sh

--- Question4.sh
+++ Question4.sh
@@ -4,6 +4,6 @@
 #Adele McCallum 10093867
 #Katherine DuQuesnay 10138898
 
-#try to sort only the third column: ie the size
-lsblk --noheadings --raw | sort -k3n
+#try to sort only the fourth column: the size
+lsblk --noheadings --raw | sort -k4n
 #we only want top 2, which will only be in G, not M

5d440a5991647883dacd2aeee5fd80e464e0fbf0
AdeleMcCallum <12adm10@queensu.ca>
2016-11-14 20:07:59 -0500

    Update Question5.sh

--- Question5.sh
+++ Question5.sh
@@ -1,1 +1,29 @@
 
+#Assignment 3 Question 5
+#Dean Wilkins-Reeves 10176758
+#Sarah Carter 10193595
+#Adele McCallum 10093867
+#Katherine DuQuesnay 10138898
+
+#!bin/bash
+
+
+
+# $ls *(.x)  <-- LISTS just exectuable files in the directory --> can this print out as an array? 
+
+# find ./ -executable -type f   <-- finds exectuable files in directory 
+
+# loop through the files found with [find ./ -executable -type f] and insert “#Developed in cisc220” just after the “#!/bin/bash” line
+
+
+#possible loop??? :
+#psuedo:
+
+#let y = $ls *(.x)
+#for (i=0; i<lengthOf(y); i++){
+    #at y[i] add "#Developed in cisc220 after the first line
+    #}
+  
+#OR!??
+
+#

566293b2dcf2ec4b6bba05cc1a5afc18bcc3b89a
AdeleMcCallum <12adm10@queensu.ca>
2016-11-14 20:40:11 -0500

    Update Question5.sh

--- Question5.sh
+++ Question5.sh
@@ -1,15 +1,14 @@
-
 #Assignment 3 Question 5
 #Dean Wilkins-Reeves 10176758
 #Sarah Carter 10193595
 #Adele McCallum 10093867
 #Katherine DuQuesnay 10138898
-
+#Adds “#Developed in cisc220” just after the “#!/bin/bash” line in all executable files in the directory
 #!bin/bash
 
 
 
-# $ls *(.x)  <-- LISTS just exectuable files in the directory --> can this print out as an array? 
+# $ls *(.x)  <-- LISTS just exectuable files in the directory -->  (is this returned as an array??) 
 
 # find ./ -executable -type f   <-- finds exectuable files in directory 
 
@@ -24,6 +23,13 @@
     #at y[i] add "#Developed in cisc220 after the first line
     #}
   
-#OR!??
-
 #
+
+#On how to add the #Developed in cisc220 comment into the files:
+
+#sed -i '8s/.*/#Developed in cisc220/'
+        #on the 8th line after title, names, explanation of script and the #!bin/bash line
+        
+        
+sed -i '8s/.*/#Developed in cisc220/' ./ -executable -type f
+        

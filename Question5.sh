64de973ef5474954a7701e10b3b1ddc8c786fd45
Sarahcarter97 <15sca@queensu.ca>
2016-11-14 22:49:29 -0500

    Update Question5.sh

--- Question5.sh
+++ Question5.sh
@@ -4,32 +4,13 @@
 #Adele McCallum 10093867
 #Katherine DuQuesnay 10138898
 #Adds “#Developed in cisc220” just after the “#!/bin/bash” line in all executable files in the directory
-#!bin/bash
+#!/bin/bash
 
-
-
-# $ls *(.x)  <-- LISTS just exectuable files in the directory -->  (is this returned as an array??) 
+#Variables required for sed to search and replace
+addition="#Developed in cisc220"
+bin="#!/bin"
+bash="/bash"
 
 # find ./ -executable -type f   <-- finds exectuable files in directory 
-
-# loop through the files found with [find ./ -executable -type f] and insert “#Developed in cisc220” just after the “#!/bin/bash” line
-
-
-#possible loop??? :
-#psuedo:
-
-#let y = $ls *(.x)
-#for (i=0; i<lengthOf(y); i++){
-    #at y[i] add "#Developed in cisc220 after the first line
-    #}
-  
-#
-
-#On how to add the #Developed in cisc220 comment into the files:
-
-#sed -i '8s/.*/#Developed in cisc220/'
-        #on the 8th line after title, names, explanation of script and the #!bin/bash line
-        
-        
-sed -i '8s/.*/#Developed in cisc220/' ./ -executable -type f
-        
+# search for all files that contain #!/bin/bash (seperated for sed to find) with seperated #!/bin/bash and the additional #Developed in cisc220
+find . -executable -type f |sed -i "s,$bin$bash,$bin$bash\n$addition"
